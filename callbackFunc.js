2597fed432969f1f4295b99a42d10d8860ae8e43
Anshu Singh <anshuonwork@gmail.com>
2024-08-03 03:05:56 +0530

    first commit

---
+++ callbackFunc.js
@@ -0,0 +1,6 @@
+//callback function
+// aisa code jo baad me chalta hai use hum ek funciton dedete hai ke bhaiya jab complete ho jana to ye fucntion chala dena , ar wo fucntion jo hum dete hai wo ek normal fnc hi hota h or use hum call back fnc kehte h
+
+setTimeout(function (){
+    console.log("2 sec delay");
+},2000); // here 2000 is milisec matlb 2 sec ke bad chalana hum mili sec me hi likhte h in term of 1000 x
