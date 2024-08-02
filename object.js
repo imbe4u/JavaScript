71d529ff585ce126a7339bfdf671ca10d0272a3b
Anshu Singh <anshuonwork@gmail.com>
2024-08-02 23:24:22 +0530

    first commit

---
+++ object.js
@@ -0,0 +1,12 @@
+//object is instance of class . or object ek bande ki details ko hold karna
+// 1 blank obj
+var a={};
+//filed obj
+var a={
+    age: 23,// here age name are props or properties
+    name:"Harsh",
+    email: "email",
+    prop: function () {}
+    //obj ke andar aisi property jiski value 'function' ho use mehtod kehte h.
+}
+console.log(a.name);
