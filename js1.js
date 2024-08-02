55c1af6098c96ce296078b989cb0f18b027034b8
Anshu Singh <anshuonwork@gmail.com>
2024-08-02 23:25:47 +0530

    second commit

--- js1.js
+++ js1.js
@@ -1,7 +1,7 @@
-//hoisting is something in which we can we variable or functions can be used before the declaration.
+//hoisting is something in which  variable or functions can be used before the declaration.
 //in hoisting declaration shifted to top of the code. var a; this is decalaration and a=2; is initialisation.
 
 console.log(a);//we will get 'undefined' because it is knowing that only declaration is done. 
 var a=12;
 
-//there is difference between undefined and not defined not defined is something which tells that the thing does not exist in the code. and undefined tells that it is defined but not initialised.
+//there is difference between undefined and not defined , not defined is something which tells that the thing does not exist in the code. and undefined tells that it is defined but not initialised.
